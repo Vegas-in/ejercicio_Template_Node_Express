375e9a2b072fd815018b7acfaeb20ed855c5cb7a
Vegas-in <angeldevegas@gmail.com>
2024-04-02 12:44:11 +0200

    estilos y manipulacion del DOM

--- index.js
+++ index.js
@@ -2,27 +2,33 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.set('view engine', 'pug');
-app.set('views','./views');
+app.set('views', './views');
 
-app.get('/home', function(req, res){
+
+app.get('/home', function (req, res) {
     res.render('home_view');
- });
- app.get('/about', function(req, res){
-    res.render('about_view');
- });
- app.get('/location', function(req, res){
-    res.render('location_view');
- });
- app.get('/mission', function(req, res){
-    res.render('mission_view');
- });
- app.get('/contact', function(req, res){
-    res.render('contact_view');
- });
+});
 
+app.get('/who-we-are', function (req, res) {
+    res.render('quienes_view');
+});
+
+app.get('/where-we-are', function (req, res) {
+    res.render('donde_view');
+});
+
+app.get('/what-we-do', function (req, res) {
+    res.render('que_view');
+});
+
+app.get('/contact-us', function (req, res) {
+    res.render('contacto_view');
+});
 
 // http://localhost:3000
 app.listen(port, () => {
     console.log(`Example app listening on  http://localhost:${port}`);
-  });
+});
